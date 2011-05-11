300fb4610ff68143deea11f833bde9eed4ad1d53
stankod <flades@gmail.com>
2011-05-11 19:14:10 +0300

    loop through all identifiers in case_sensitive

--- Problem5/solution.py
+++ Problem5/solution.py
@@ -1,30 +1,28 @@
 import re
 
-number = r'(?:[-\+]?(?:\b\d+\.\d*|\.\d+\b|\b\d+\b))'
-string = r'".*"'
-tokens_rest = r"[\(\)']|#t|#f"
-special_symbol = r'\!\$\%\&\*\+\-\.\/\:\<\=\>\?\@\^\_\~'
-special_symbol = r'!%&-/:<=>@_~\$\*\+\.\?\^'
-special_symbol = re.escape(r'!$%&*+-./:<=>?@^_~')
-identifier = r'\A[a-zA-Z][{1}a-zA-Z{0}]*|\B[{0}]\B'.format(special_symbol, number)
-identifier = r'\b[a-zA-Z][a-zA-Z0-9{0}]*|(?=\W|^)[{0}](?=\W|$)?|\b_'.format(special_symbol)
+_number = r'(?:[-\+]?(?:\b\d+\.\d*|\.\d+\b|\b\d+\b))'
+_string = r'".*?"'
+_tokens = r"[\(\)']|#t|#f"
+_special_symbol = re.escape(r'!$%&*+-./:<=>?@^_~')
+_identifier = r'\b[a-zA-Z][a-zA-Z0-9{0}]*|(?=\W|^)[{0}](?=\W|$)?|\b_'.format(_special_symbol)
 
 def tokenize(code):
-    tokens = re.findall(r'|'.join([tokens_rest, string, number, identifier]),
-            code, re.S)
-    return tokens
+    tokens = r'|'.join([_tokens, _string, _number, _identifier])
+    return re.findall(tokens, code, re.S)
 
-def identifiers(tokens):
+def identifiers(tokens, caseinsensitive=True):
     idents = set()
     for token in tokens:
-        iden = re.match(identifier, token)
-        if iden and all([iden.group().lower() != found.lower()\
-                for found in idents]):
-            idents.add(iden.group())
+        iden = re.match(_identifier, token)
+        if iden:
+            iden = iden.group()
+            if not caseinsensitive or\
+                    iden.lower() not in [found.lower() for found in idents]:
+                idents.add(iden)
     return idents
 
 def case_sensitive(code, identifs):
-    idents_in_code = identifiers(tokenize(code))
+    idents_in_code = identifiers(tokenize(code), False)
     replacements = {iden.lower() : iden for iden in identifs}
     for ident in idents_in_code:
         if replacements.get(ident.lower()):
