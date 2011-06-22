6b75d8b432d9dd2fa21cab6892399388e47ec4bc
stankod <flades@gmail.com>
2011-06-22 12:25:24 +0300

    tests import fixed

--- Problem9/sample_test_ek.py
+++ Problem9/sample_test_ek.py
@@ -1,112 +1,72 @@
-from solution import multimethod
 import unittest
-
-@multimethod
-def a_func(a:int, b:int):return a**b
-
-@a_func.multimethod
-def a_func(a:float, b:float):return a/b
-
-@a_func.multimethod
-def a_func(a:str, b:int):return a*b
-
-@a_func.multimethod
-def a_func(a:str, b:str):return ' '.join([a,b])
-
-@a_func.multimethod
-def a_func(a,b):return a,b
-
-class song:
-    @multimethod
-    def sing(self, a:list):
-        return 'Every list is sacred'
-
-    @sing.multimethod
-    def sing(self, a:set):
-        return 'Every set is great'
-
-    @sing.multimethod
-    def sing(self, a:dict):
-        return 'If a dict is wasted'
-
-    @sing.multimethod
-    def sing(self, a:tuple):
-        return 'God gets quite upset'
+from solution import multidispatch
 
 
-@multimethod
-def da_func(a:int, b:float):pass
+class multi_baba(metaclass=multidispatch):
+    def __init__(self):
+        self.i = 6
+        self.f = 7.0
+        self.s = 'baba'
+        self.c = 42j
+    def m(self, arg:int):
+        return self.i
+    def m(self, arg:float):
+        return self.f
+    def m(self, arg:str):
+        return self.s
+    def m(self, arg:complex):
+        return self.c
 
-@da_func.multimethod
-def da_func(a:float, b:int):pass
 
-class TestMultimethods(unittest.TestCase):
-    s = song()
-    def test_simple(self):
-        self.assertEqual(3125, a_func(5,5))
-        self.assertAlmostEqual(150.13/234.52, a_func(150.13,234.52))
-        self.assertEqual('babababababa', a_func('ba',6))
-        self.assertEqual('e bre de bre', a_func('e bre','de bre'))
-        self.assertEqual(([42],'Jeremy'), a_func([42], 'Jeremy'))
+class unanotated(metaclass=multidispatch):
+    def __init__(self):pass
+    def m(self, a:int):return int
+    def m(self, a:float):return float
+    def m(self, a:str):return str
+    def m(self, a):return 'OMG!!!'
 
-    def test_multimethod(self):
-        self.assertEqual('Every list is sacred', self.s.sing([]))
-        self.assertEqual('Every set is great', self.s.sing(set()))
-        self.assertEqual('If a dict is wasted', self.s.sing({}))
-        self.assertEqual('God gets quite upset', self.s.sing(()))
 
-    def test_name_error(self):
-        with self.assertRaises(NameError):
-            @da_func.multimethod
-            def larodi():pass
+class several_multi(metaclass=multidispatch):
+    def __init__(self):pass
+    def m1(self, a:int): return 'm1-int'
+    def m1(self, a:float): return 'm1-float'
+    def m2(self, b:int): return 'm2-int'
+    def m2(self, b:float): return 'm2-float'
 
-    def test_lookup_error(self):
+class should_raise(metaclass=multidispatch):
+    def __init__(self):pass
+    def m(self, a:int):pass
+
+
+class TestMultiDispatch(unittest.TestCase):
+    def test_multidispatch_anotated(self):
+        multi_b = multi_baba()
+
+        self.assertIsInstance(multi_b.m(0), int)
+        self.assertIsInstance(multi_b.m(12.5), float)
+        self.assertIsInstance(multi_b.m(''), str)
+        self.assertIsInstance(multi_b.m(2j), complex)
+
+    def test_work_with_unanotated(self):
+        unanot = unanotated()
+
+        self.assertEqual(int, unanot.m(5))
+        self.assertEqual(float, unanot.m(12.0))
+        self.assertEqual(str, unanot.m('baba'))
+        self.assertEqual('OMG!!!', unanot.m(1+2j))
+
+    def test_raise_lookup_error(self):
+        raises = should_raise()
         with self.assertRaises(LookupError):
-            self.s.sing(17)
+            raises.m('This parrot is dead')
 
-    @unittest.expectedFailure
-    def test_type_error(self):
-        with self.assertRaises(TypeError):
-            @multimethod
-            class something_completely_different:
-                pass
 
-#############################
-#Sample test from assignment#
-#############################
-class MultiDispatchTest(unittest.TestCase):
-    def test_single_argument_two_independant_types(self):
-        class Spam:
-            def __init__(self, value):
-                self.value = value
-
-            @multimethod
-            def eggs(self, arg: int):
-                return self.value * arg
-
-            @eggs.multimethod
-            def eggs(self, arg: str):
-                return self.value + arg
-
-        self.assertEqual(10, Spam(2).eggs(5))
-        self.assertEqual('spam!',  Spam('spam').eggs('!'))
-
-    def test_invoking_with_a_subclass(self):
-        class Spam:
-            @multimethod
-            def eggs(self, arg: object):
-                return 'object'
-
-        self.assertEqual('object', Spam().eggs(12))
-
-    def test_cannot_dispatch(self):
-        class Spam:
-            @multimethod
-            def eggs(self, arg: int): pass
-
-        with self.assertRaises(LookupError):
-            Spam().eggs('')
-
+    def test_several_multimethods(self):
+        sm = several_multi()
+        self.assertEqual('m1-int', sm.m1(0))
+        self.assertEqual('m1-float', sm.m1(0.0))
+        self.assertEqual('m2-int', sm.m2(0))
+        self.assertEqual('m2-float', sm.m2(0.0))
 
 if __name__ == '__main__':
     unittest.main()
--- Problem9/sample_test_np.py
+++ Problem9/sample_test_np.py
@@ -1,31 +1,18 @@
 import unittest
-from solution import multimethod
+from solution import multidispatch
 
 class MultiDispatchTestNP(unittest.TestCase):
-    def test_1(self):
-        with self.assertRaises(NameError):
-            class Spam:
-                @multimethod
-                def eggs(self, arg: int): pass
-
-                @eggs.multimethod
-                def x(self, arg:str): pass
-
-            Spam().eggs('fsdfsd')
-
     def test_2(self):
-        class Spam:
-            @multimethod
+        class Spam(metaclass=multidispatch):
             def eggs(self, x: int, y: str):
                 return 42
 
-        self.assertEqual(42, Spam().eggs(y='dsf',x=5))
         with self.assertRaises(LookupError):
             Spam().eggs(y=45.463, x=14.3)
+        #self.assertEqual(42, Spam().eggs(y='dsf',x=5))
 
     def test_3(self):
-        class Spam:
-            @multimethod
+        class Spam(metaclass=multidispatch):
             def eggs(self, x:int):
                 return 42
 
